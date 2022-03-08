6074b06f989d42b9fb002da6036c55dc6c97f742
Lottie Carr <lottie.isabelle@gmail.com>
2022-03-08 22:57:27 +0000

    Adding settings
    begun adding setting controller with buttons for button labels

---
+++ Assets/Scripts/ChangeSetting.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSetting : MonoBehaviour
+{
+    [SerializeField] private Image _img;
+    [SerializeField] private Sprite _default, _pressed;
+
+    public bool setting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void change()
+    {
+        
+    }
+
+
+}
