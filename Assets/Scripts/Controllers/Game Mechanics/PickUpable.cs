3b913d3c0e70b13ef75f533270a1ef4efee7741c
Lottie Carr <lottie.isabelle@gmail.com>
2022-03-13 19:21:01 +0000

    Fix P icon display

--- Assets/Scripts/Controllers/Game Mechanics/PickUpable.cs
+++ Assets/Scripts/Controllers/Game Mechanics/PickUpable.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PickUpable : MonoBehaviour
 {   
     private string level;
+    private bool handsEmpty;
 
     void Start()
     {
@@ -16,7 +17,31 @@ public class PickUpable : MonoBehaviour
         GetComponent<BoxCollider2D>().isTrigger = true;
     }
 
+    void Update()
+    {
+        // if this box is being held, then player hands are not empty
+        if(this.transform.parent.GetComponent<BoxController>().isHeld){
+            handsEmpty = false;
+        } else {
+            // otherwise need to get hands empty bool from player - box is in boxes in hierarchy
+            handsEmpty = this.transform.parent.parent.parent.GetChild(5).GetComponent<PlayerController>().handsEmpty;
+
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
+    {   
+        // show interact icon p
+        if(collision.CompareTag("Player")){
+            if(level != "Tutorial2" && !this.transform.parent.GetComponent<BoxController>().isHeld)
+            {
+                this.transform.parent.parent.parent.GetChild(1).GetComponent<IconController>().show();
+            }
+        }
+     
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {   
         // show interact icon p
         if(collision.CompareTag("Player")){
@@ -32,7 +57,11 @@ public class PickUpable : MonoBehaviour
     {
         // hide interact icon p
         if(collision.CompareTag("Player")){
-            if(level != "Tutorial2" && !this.transform.parent.GetComponent<BoxController>().isHeld)
+            // not on tutorial part2 and only when hands are empty
+
+
+            //if(level != "Tutorial2" && !this.transform.parent.GetComponent<BoxController>().isHeld)
+            if(level != "Tutorial2" && handsEmpty)
             {
                 this.transform.parent.parent.parent.GetChild(1).GetComponent<IconController>().hide();
             }
