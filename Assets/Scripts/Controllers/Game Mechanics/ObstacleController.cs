a90cfe73c9d87e89b8fcf5aa79a58d840f10c590
Lottie Carr <lottie.isabelle@gmail.com>
2022-04-13 16:36:00 +0100

    add transparency for platforms and obstacles

--- Assets/Scripts/Controllers/Game Mechanics/ObstacleController.cs
+++ Assets/Scripts/Controllers/Game Mechanics/ObstacleController.cs
@@ -9,6 +9,12 @@ public class ObstacleController : MonoBehaviour
 
     [SerializeField] public TextMeshProUGUI taskText;
 
+    private Color solidObs;
+    private Color transparentObs;
+
+    private Color solidText;
+    private Color transparentText;
+
     private bool hasTrigger;
     private int numBoxes;
 
@@ -16,16 +22,27 @@ public class ObstacleController : MonoBehaviour
 
     void Start()
     {
+        solidObs = new Color(1f,1f,1f,1f);
+        transparentObs = new Color(1f,1f,1f,0.5f);
+
+        solidText = new Color(0f,0.1738f,0.2924f,1f);
+        transparentText = new Color(0f,0.1738f,0.2924f,0.5f);
+
+        taskText.gameObject.layer = 5;
+
+
         if(this.transform.childCount == 0){
             hasTrigger = false;
         } else {
             hasTrigger = true;
 
             if(isVisible){
+                this.transform.GetComponent<SpriteRenderer>().color = solidObs;
                 // disable trigger box collider on start
                 this.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = false;
                 this.transform.GetChild(0).GetComponent<BoxCollider2D>().isTrigger = false;
             } else {
+                this.transform.GetComponent<SpriteRenderer>().color = transparentObs;
                 // enable trigger box collider on start
                 this.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = true;
                 this.transform.GetChild(0).GetComponent<BoxCollider2D>().isTrigger = true;
@@ -64,9 +81,10 @@ public class ObstacleController : MonoBehaviour
 
         }
 
-        this.transform.GetComponent<SpriteRenderer>().enabled = true;
+        this.transform.GetComponent<SpriteRenderer>().color = solidObs;
         this.transform.GetComponent<BoxCollider2D>().enabled = true;
-        taskText.enabled = true;
+        taskText.color = solidText;
+        
         isVisible = true;
         
     }
@@ -93,9 +111,10 @@ public class ObstacleController : MonoBehaviour
             this.transform.GetChild(0).GetComponent<BoxCollider2D>().isTrigger = true;
         }
 
-        this.transform.GetComponent<SpriteRenderer>().enabled = false;
+        this.transform.GetComponent<SpriteRenderer>().color = transparentObs;
         this.transform.GetComponent<BoxCollider2D>().enabled = false;
-        taskText.enabled = false;
+        taskText.color = transparentText;
+        
         isVisible = false;
 
     }
--- Assets/Scripts/Controllers/Game Mechanics/PlatformController.cs
+++ Assets/Scripts/Controllers/Game Mechanics/PlatformController.cs
@@ -10,25 +10,38 @@ public class PlatformController : MonoBehaviour
 
     private int num;
 
+    private Color solidPlat;
+    private Color transparentPlat;
+
+    private Color solidText;
+    private Color transparentText;
+
     void Start()
     {
         // count number of children to hide
         num = this.transform.childCount;
+
+        solidPlat = new Color(1f,1f,1f,1f);
+        transparentPlat = new Color(1f,1f,1f,0.5f);
+
+        solidText = new Color(0f,0.1738f,0.2924f,1f);
+        transparentText = new Color(0f,0.1738f,0.2924f,0.5f);
     }
 
     public void show()
     {
         this.GetComponent<BoxCollider2D>().enabled = true;
-        taskText.enabled = true;
+        taskText.color = solidText;
+        
         isVisible = true;
 
         if(num == 0){
-            this.GetComponent<SpriteRenderer>().enabled = true;
+            this.GetComponent<SpriteRenderer>().color = solidPlat;
             
         } else {
 
             for(int i = 0; i < num; i++){
-                this.transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = true;
+                this.transform.GetChild(i).GetComponent<SpriteRenderer>().color = solidPlat;
             }
 
         }
@@ -38,16 +51,17 @@ public class PlatformController : MonoBehaviour
     public void hide()
     {
         this.GetComponent<BoxCollider2D>().enabled = false;
-        taskText.enabled = false;
+        taskText.color = transparentText;
+
         isVisible = false;
 
         if(num == 0){
-            this.GetComponent<SpriteRenderer>().enabled = false;
+            this.GetComponent<SpriteRenderer>().color = transparentPlat;
             
         } else {
 
             for(int i = 0; i < num; i++){
-                this.transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = false;
+                this.transform.GetChild(i).GetComponent<SpriteRenderer>().color = transparentPlat;
             }
 
         }
